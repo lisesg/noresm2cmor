f450d72df0fb86e7f916c74863dfeb87a85dfc33
Lise Seland Graff <lisesg@login3-nird-lmd.nird.login.svc.cluster.local>
2024-01-03 14:04:10 +0100

    Updated cmorLink.sh and CMIP6_CV.json. New and updated namelists for mutiple KeyCLIM cases. The main cases are now set up so that the default CMIP6 output is cmorized; the main cases are hist-cloud2, hist-eddy, hist-iceSheet3, hist-snow, hist-ozone, hist-piAerOxid, ssp585-cloud2, ssp585-eddy, ssp585-iceSheet3, ssp585, snow, ssp585-ozone, ssp585-piAerOxid.

---
+++ namelists/KeyCLIM_NorESM2-MM/ssp370-cloud2/cmor_a.sh
@@ -0,0 +1,74 @@
+##!/bin/bash
+
+CMOR_ROOT=$(cd $(dirname $0) && cd ../../.. && pwd)
+source ${CMOR_ROOT}/workflow/cmorRun1memb.sh
+
+# initialize
+login0=false
+
+# set active
+login0=true
+
+# initialize
+version=v20220113a
+expid=ssp370-cloud2
+model=NorESM2-MM
+
+# --- Use input arguments if exits
+if [ $# -ge 1 ] 
+then
+     while test $# -gt 0; do
+         case "$1" in
+             -m=*)
+                 model=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -e=*)
+                 expid=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -v=*)
+                 version=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             * )
+                 echo "ERROR: option $1 not allowed."
+                 echo "*** EXITING THE SCRIPT"
+                 exit 1
+                 ;;
+         esac
+    done
+fi
+# --- 
+
+echo "--------------------"
+echo "EXPID: $expid       "
+echo "--------------------"
+
+echo "                    "
+echo "START CMOR...       "
+echo "$(date)             "
+echo "                    "
+
+if $login0
+then
+#----------------
+# ssp370
+#----------------
+CaseName=NSSP370frc2_f09_tn14_cloudxpice_220812
+real=1
+years1=(2014 $(seq 2021 10 2091))
+years2=(2020 $(seq 2030 10 2100))
+
+runcmor -c=$CaseName -m=$model -e=$expid -v=$version -r=$real -yrs1="${years1[*]}" -yrs2="${years2[*]}" -mpi=UMPI
+
+#---
+fi
+#---
+
+wait
+echo "         "
+echo "CMOR DONE"
+echo "$(date)  "
+echo "~~~~~~~~~"
+
---
+++ namelists/KeyCLIM_NorESM2-MM/ssp585-cloud2/cmor_b.sh
@@ -0,0 +1,74 @@
+##!/bin/bash
+
+CMOR_ROOT=$(cd $(dirname $0) && cd ../../.. && pwd)
+source ${CMOR_ROOT}/workflow/cmorRun1memb.sh
+
+# initialize
+login0=false
+
+# set active
+login0=true
+
+# initialize
+version=v20220113b
+expid=ssp585-cloud2
+model=NorESM2-MM
+
+# --- Use input arguments if exits
+if [ $# -ge 1 ] 
+then
+     while test $# -gt 0; do
+         case "$1" in
+             -m=*)
+                 model=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -e=*)
+                 expid=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -v=*)
+                 version=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             * )
+                 echo "ERROR: option $1 not allowed."
+                 echo "*** EXITING THE SCRIPT"
+                 exit 1
+                 ;;
+         esac
+    done
+fi
+# --- 
+
+echo "--------------------"
+echo "EXPID: $expid       "
+echo "--------------------"
+
+echo "                    "
+echo "START CMOR...       "
+echo "$(date)             "
+echo "                    "
+
+if $login0
+then
+#----------------
+# ssp585
+#----------------
+CaseName=NSSP585frc2_f09_tn14_cloudxpice_220819
+real=1
+years1=(2061 $(seq 2071 10 2091))
+years2=(2070 $(seq 2080 10 2100))
+
+runcmor -c=$CaseName -m=$model -e=$expid -v=$version -r=$real -yrs1="${years1[*]}" -yrs2="${years2[*]}" -mpi=UMPI
+
+#---
+fi
+#---
+
+wait
+echo "         "
+echo "CMOR DONE"
+echo "$(date)  "
+echo "~~~~~~~~~"
+
